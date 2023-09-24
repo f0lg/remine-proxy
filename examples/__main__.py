a34d70c927341ac84632f50a89490b6dcdf3d1ea
Null-vg <144524322+Null-vg@users.noreply.github.com>
2023-09-24 21:55:35 +0900

    Updated

--- examples/__main__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import os
-
-if __name__ == '__main__':
-    print("Examples:")
-    for filename in sorted(os.listdir(os.path.dirname(__file__))):
-        name, ext = os.path.splitext(filename)
-        if ext == '.py' and name[0] != '_':
-            print("- examples.%s" % name)
